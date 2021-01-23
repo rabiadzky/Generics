714182aa790ea08c94a75731bcbebe54a1f79163
rabia.dzky@hotmail.com <rabia.dzky@hotmail.com>
2021-01-23 20:23:34 +0300

    Add project files.

---
+++ Generics/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> sehirler = new List<string>();
+            sehirler.Add("Sivas");
+            sehirler.Add("Bolu");
+            Console.WriteLine(sehirler.Count);
+
+            MyList<string> sehirler2 = new MyList<string>();
+            sehirler2.Add("Bolu");
+            sehirler2.Add("Sivas");
+            sehirler2.Add("Trabzon");
+            Console.WriteLine(sehirler2.Count);
+                
+        }
+    }
+    class MyList<T> //Generic Class
+    {
+        T[] array;
+        T[] tempArray;
+        public MyList()
+        {
+            array = new T[0];
+        }
+        public void Add(T item)
+        {
+            tempArray = array;
+            array = new T[array.Length + 1];
+            for (int i = 0; i <tempArray.Length; i++)
+            {
+                array[i] = tempArray[i];
+            }
+            array[array.Length - 1] = item;
+        }
+
+        public int Count
+        {
+            get { return array.Length; }
+           
+        }
+
+    }
+}
